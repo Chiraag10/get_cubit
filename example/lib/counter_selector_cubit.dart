78885d2f4da366cd04f5b54ad468919e89dc31b5
chiraag06 <chiraag@codewave.com>
2024-03-27 18:57:19 +0530

    deleted example folder

--- example/lib/counter_selector_cubit.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-class CounterSelectorCubit extends Cubit<int> {
-  CounterSelectorCubit() : super(0);
-
-  void selectCounter(int i) => emit(i);
-}
